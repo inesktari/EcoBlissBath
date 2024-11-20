8975aaa5b7b074a0963f5c5ba47dca0eda3e5aed
inesktari <135965289+inesktari@users.noreply.github.com>
2024-11-20 13:46:24 +0100

    Login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,11 +3,34 @@ const password = Cypress.env("password");
 const baseURL = Cypress.env("baseURL");
 
 describe("login page", () => {
-  it("test login form", () => {
+  it("should successfully log in with valid ID", () => {
     cy.visit(baseURL + "login");
     cy.getBySel("login-input-username").type(username);
     cy.getBySel("login-input-password").type(password);
     cy.getBySel("login-submit").click();
-    cy.get("nav").should("contain", "panier");
+    cy.get("nav").should("contain", "Déconnexion");
+  });
+
+  it("shouldn't log in with invalid email, invalid password, and display error message", () => {
+    cy.visit(baseURL + "login");
+    cy.getBySel("login-input-username").type("invalidUsername@username.com");
+    cy.getBySel("login-input-password").type("invalidPassword");
+    cy.getBySel("login-submit").click();
+    cy.get("nav").should("contain", "Connexion");
+    cy.getBySel("login-errors").should("exist");
+    cy.get("form").should("contain", "Identifiants incorrects");
+  });
+
+  it("shouldn't log in with a username different of an email adress, invalid password, and display error message", () => {
+    cy.visit(baseURL + "login");
+    cy.getBySel("login-input-username").type("invalidUsername");
+    cy.getBySel("login-input-password").type("invalidPassword");
+    cy.getBySel("login-submit").click();
+    cy.get("nav").should("contain", "Connexion");
+    cy.getBySel("login-errors").should("exist");
+    cy.get("form").should(
+      "contain",
+      "Merci de remplir correctement tous les champs"
+    );
   });
 });
