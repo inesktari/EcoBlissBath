ffbb4da68cb49d011e256eab7d7e508e559f86fa
inesktari <135965289+inesktari@users.noreply.github.com>
2024-11-21 13:33:33 +0100

    Login tests with faker

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,9 @@
+import { faker } from "@faker-js/faker";
+
 const username = Cypress.env("username");
 const password = Cypress.env("password");
+const invalidUsername = faker.internet.email();
+const invalidPassword = faker.internet.password();
 const baseURL = Cypress.env("baseURL");
 
 describe("login / logout", () => {
@@ -24,21 +28,56 @@ describe("login / logout", () => {
 describe("Error messages on login", () => {
   it("shouldn't log in with invalid email, invalid password, and display error message", () => {
     cy.visit(baseURL + "login");
-    cy.getBySel("login-input-username").type("invalidUsername@username.com");
-    cy.getBySel("login-input-password").type("invalidPassword");
+    cy.getBySel("login-input-username").type(invalidUsername);
+    cy.getBySel("login-input-password").type(invalidPassword);
     cy.getBySel("login-submit").click();
     cy.get("nav").should("contain", "Connexion");
     cy.getBySel("login-errors").should("exist");
+    cy.get('label[for="username"]').should("have.class", "error");
+    cy.get('label[for="password"]').should("have.class", "error");
+    cy.getBySel("login-errors").should("have.class", "error");
     cy.get("form").should("contain", "Identifiants incorrects");
   });
 
   it("shouldn't log in with a username different of an email adress, invalid password, and display error message", () => {
     cy.visit(baseURL + "login");
     cy.getBySel("login-input-username").type("invalidUsername");
-    cy.getBySel("login-input-password").type("invalidPassword");
+    cy.getBySel("login-input-password").type(invalidPassword);
     cy.getBySel("login-submit").click();
     cy.get("nav").should("contain", "Connexion");
     cy.getBySel("login-errors").should("exist");
+    cy.get('label[for="username"]').should("have.class", "error");
+    cy.get('label[for="password"]').should("not.have.class", "error");
+    cy.getBySel("login-errors").should("have.class", "error");
+    cy.get("form").should(
+      "contain",
+      "Merci de remplir correctement tous les champs"
+    );
+  });
+
+  it("shouldn't log in with an empty password, invalid password, and display error message", () => {
+    cy.visit(baseURL + "login");
+    cy.getBySel("login-input-username").type(invalidUsername);
+    cy.getBySel("login-submit").click();
+    cy.get("nav").should("contain", "Connexion");
+    cy.getBySel("login-errors").should("exist");
+    cy.get('label[for="username"]').should("not.have.class", "error");
+    cy.get('label[for="password"]').should("have.class", "error");
+    cy.getBySel("login-errors").should("have.class", "error");
+    cy.get("form").should(
+      "contain",
+      "Merci de remplir correctement tous les champs"
+    );
+  });
+
+  it("shouldn't log in with an empty username and an empty password, and display error message", () => {
+    cy.visit(baseURL + "login");
+    cy.getBySel("login-submit").click();
+    cy.get("nav").should("contain", "Connexion");
+    cy.getBySel("login-errors").should("exist");
+    cy.get('label[for="username"]').should("have.class", "error");
+    cy.get('label[for="password"]').should("have.class", "error");
+    cy.getBySel("login-errors").should("have.class", "error");
     cy.get("form").should(
       "contain",
       "Merci de remplir correctement tous les champs"
