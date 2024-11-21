f16ed4c1bae81d97cd04db8070c6f705085b8b8f
inesktari <135965289+inesktari@users.noreply.github.com>
2024-11-21 15:59:10 +0100

    Test API user

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,19 +1,62 @@
+import { faker } from "@faker-js/faker";
+
 const username = Cypress.env("username");
 const password = Cypress.env("password");
 const baseURL = Cypress.env("baseURL");
 const apiURL = Cypress.env("apiURL");
-const token = "aaaaaaaaaaaaaaa";
+const invalidToken = faker.string.alphanumeric(100);
 
-describe("API orders", () => {
-  it("test GET user orders", () => {
+describe("API User", () => {
+  it("shouldn't get user informations without authentification != 200", () => {
     cy.request({
       method: "GET",
-      url: apiURL + "orders",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+      url: apiURL + "me",
+      failOnStatusCode: false,
     }).then((response) => {
-      expect(response.status).to.eq(200);
+      expect(response.status).to.not.eq(200);
+      // expect(response.status).to.eq(404);                   // n'est pas spécifié dans la documentation de l'API
+    });
+  });
+
+  it("shouldn't get user informations with fake token = 404", () => {
+    cy.request({
+      method: "POST",
+      url: apiURL + "login",
+      headers: {
+        Authorization: "Bearer " + `${invalidToken}`,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.not.eq(200);
+      expect(response.status).to.eq(404); // n'est pas spécifié dans la documentation de l'API
+    });
+  });
+
+  it("should get user informations after authentification => 200", () => {
+    cy.request({
+      method: "POST",
+      url: apiURL + "login",
+      body: {
+        username: username,
+        password: password,
+      },
+      failOnStatusCode: false,
+    }).then((loginResponse) => {
+      expect(loginResponse.status).to.eq(200);
+      expect(loginResponse.body).to.have.property("token");
+      Cypress.env("token", loginResponse.body.token);
+
+      cy.request({
+        method: "GET",
+        url: apiURL + "me",
+        headers: {
+          Authorization: "Bearer " + Cypress.env("token"),
+        },
+        failOnStatusCode: false,
+      }).then((meResponse) => {
+        expect(meResponse.status).to.eq(200);
+        expect(meResponse.body).to.have.property("email");
+      });
     });
   });
 });
