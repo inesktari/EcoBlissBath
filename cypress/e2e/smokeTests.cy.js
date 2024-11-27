54dc88f3be8706fae4b6b75494062db850568c98
inesktari <135965289+inesktari@users.noreply.github.com>
2024-11-28 00:40:08 +0100

    Smoke tests

--- cypress/e2e/smokeTests.cy.js
+++ cypress/e2e/smokeTests.cy.js
@@ -1,12 +1,30 @@
 const username = Cypress.env("username");
 const password = Cypress.env("password");
 const baseURL = Cypress.env("baseURL");
+const apiURL = Cypress.env("apiURL");
 
-describe("login page", () => {
+describe("Login page", () => {
   it("navigate to login page and verify if the elements exist", () => {
     cy.visit(baseURL + "login");
-    cy.getBySel("login-input-username").should("exist");
-    cy.getBySel("login-input-password").should("exist");
-    cy.getBySel("login-submit").should("exist");
+    cy.getBySel("login-input-username").should("be.visible");
+    cy.getBySel("login-input-password").should("be.visible");
+    cy.getBySel("login-submit").should("be.visible");
+  });
+});
+
+describe("Products page", () => {
+  it("navigate to the products page and verify if the elements exist", () => {
+    cy.visit(baseURL + "/products");
+    cy.getBySel("product").should("be.visible");
+    cy.getBySel("product-link").should("be.visible");
+  });
+});
+
+describe("Product sheet", () => {
+  it("navigate to a product sheet and verify if the elements exist", () => {
+    cy.visit(baseURL + "/products");
+    cy.getBySel("product-link").first().click();
+    cy.getBySel("detail-product-add").should("be.visible");
+    cy.getBySel("detail-product-stock").should("be.visible");
   });
 });
