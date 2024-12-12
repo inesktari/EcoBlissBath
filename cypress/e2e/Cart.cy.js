eb7c1dbb8cf2f224d9785ffad986d7c69bcab7fc
inesktari <135965289+inesktari@users.noreply.github.com>
2024-12-12 22:31:26 +0100

    Cart tests - Step 5

--- cypress/e2e/Cart.cy.js
+++ cypress/e2e/Cart.cy.js
@@ -3,93 +3,93 @@ const password = Cypress.env("password");
 const baseURL = Cypress.env("baseURL");
 const apiURL = Cypress.env("apiURL");
 
-// describe("Display of the add to cart button on the sheets product", () => {
-//   before(() => {
-//     Cypress.env("token", null);
-//   });
+describe("Display of the add to cart button on the sheets product", () => {
+  before(() => {
+    Cypress.env("token", null);
+  });
 
-//   it("Shouldn't be visible if the value of the stock is < 1", () => {
-//     const problematicProducts = [];
+  it("Shouldn't be visible if the value of the stock is < 1", () => {
+    const problematicProducts = [];
 
-//     cy.visit(baseURL + "login");
-//     cy.getBySel("login-input-username").type(username);
-//     cy.getBySel("login-input-password").type(password);
-//     cy.getBySel("login-submit").click();
-//     cy.wait(2000);
-//     cy.visit(baseURL + "products");
-//     cy.wait(2000);
+    cy.visit(baseURL + "login");
+    cy.getBySel("login-input-username").type(username);
+    cy.getBySel("login-input-password").type(password);
+    cy.getBySel("login-submit").click();
+    cy.wait(2000);
+    cy.visit(baseURL + "products");
+    cy.wait(2000);
 
-//     cy.get(".list-products")
-//       .find("article.mini-product")
-//       .its("length")
-//       .then((length) => {
-//         for (let i = 0; i < length; i++) {
-//           cy.get(".list-products")
-//             .find("article.mini-product")
-//             .eq(i)
-//             .find('[data-cy="product-link"]')
-//             .click();
+    cy.get(".list-products")
+      .find("article.mini-product")
+      .its("length")
+      .then((length) => {
+        for (let i = 0; i < length; i++) {
+          cy.get(".list-products")
+            .find("article.mini-product")
+            .eq(i)
+            .find('[data-cy="product-link"]')
+            .click();
 
-//           cy.wait(800);
-//           cy.get('[data-cy="detail-product-name"]').should("be.visible");
+          cy.wait(800);
+          cy.get('[data-cy="detail-product-name"]').should("be.visible");
 
-//           cy.wait(800);
-//           cy.get("p.stock")
-//             .invoke("text")
-//             .then((text) => {
-//               const match = text.match(/-?\d+/); // Récupère le nombre dans le texte
-//               const stockValue = match ? Number(match[0]) : null;
+          cy.wait(800);
+          cy.get("p.stock")
+            .invoke("text")
+            .then((text) => {
+              const match = text.match(/-?\d+/); // Récupère le nombre dans le texte
+              const stockValue = match ? Number(match[0]) : null;
 
-//               cy.wait(800);
-//               cy.get('[data-cy="detail-product-name"]')
-//                 .invoke("text")
-//                 .then((productName) => {
-//                   if (stockValue === null) {
-//                     problematicProducts.push({
-//                       productName,
-//                       reason: "Stock not displayed",
-//                     });
-//                   } else if (stockValue < 1) {
-//                     cy.wait(800);
-//                     cy.get('[data-cy="detail-product-add"]').then(($btn) => {
-//                       if ($btn.is(":visible")) {
-//                         problematicProducts.push({
-//                           productName,
-//                           reason: "Add to cart button visible with stock < 1",
-//                         });
-//                       }
-//                     });
-//                   }
-//                 });
-//             });
+              cy.wait(800);
+              cy.get('[data-cy="detail-product-name"]')
+                .invoke("text")
+                .then((productName) => {
+                  if (stockValue === null) {
+                    problematicProducts.push({
+                      productName,
+                      reason: "Stock not displayed",
+                    });
+                  } else if (stockValue < 1) {
+                    cy.wait(800);
+                    cy.get('[data-cy="detail-product-add"]').then(($btn) => {
+                      if ($btn.is(":visible")) {
+                        problematicProducts.push({
+                          productName,
+                          reason: "Add to cart button visible with stock < 1",
+                        });
+                      }
+                    });
+                  }
+                });
+            });
 
-//           cy.wait(600);
-//           cy.visit(baseURL + "products");
+          cy.wait(600);
+          cy.visit(baseURL + "products");
 
-//           cy.wait(800);
-//           cy.get(".list-products").should("be.visible");
-//           cy.wait(800);
-//         }
-//       })
-//       .then(() => {
-//         if (problematicProducts.length > 0) {
-//           cy.log("Produits avec un problème :");
-//           problematicProducts.forEach((product) => {
-//             cy.log(`${product.productName} - ${product.reason}`);
-//           });
-//           throw new Error(
-//             `List of products which mistakenly contains the add to cart button : ${problematicProducts
-//               .map((p) => `${p.productName} (${p.reason})`)
-//               .join(", ")}`
-//           );
-//         } else {
-//           cy.log(
-//             "The add to cart button is displayed correctly in the sheet of all products."
-//           );
-//         }
-//       });
-//   });
-// });
+          cy.wait(800);
+          cy.get(".list-products").should("be.visible");
+          cy.wait(800);
+        }
+      })
+      .then(() => {
+        if (problematicProducts.length > 0) {
+          cy.log("Produits avec un problème :");
+          problematicProducts.forEach((product) => {
+            cy.log(`${product.productName} - ${product.reason}`);
+          });
+          throw new Error(
+            `List of products which mistakenly contains the add to cart button : ${problematicProducts
+              .map((p) => `${p.productName} (${p.reason})`)
+              .join(", ")}`
+          );
+        } else {
+          cy.log(
+            "The add to cart button is displayed correctly in the sheet of all products."
+          );
+        }
+      });
+  });
+});
 
 describe("Adding a product to the cart", () => {
   before(() => {
@@ -197,98 +197,98 @@ describe("Adding a product to the cart", () => {
   });
 });
 
-// describe("Adding a product to cart under boundary conditions", () => {
-//   before(() => {
-//     Cypress.env("token", null);
-//   });
+describe("Adding a product to cart under boundary conditions", () => {
+  before(() => {
+    Cypress.env("token", null);
+  });
 
-//   it("Should not allow adding a negative quantity to the cart", () => {
-//     cy.visit(baseURL + "login");
-//     cy.getBySel("login-input-username").type(username);
-//     cy.getBySel("login-input-password").type(password);
-//     cy.getBySel("login-submit").click();
-//     cy.wait(2000);
+  it("Should not allow adding a negative quantity to the cart", () => {
+    cy.visit(baseURL + "login");
+    cy.getBySel("login-input-username").type(username);
+    cy.getBySel("login-input-password").type(password);
+    cy.getBySel("login-submit").click();
+    cy.wait(2000);
 
-//     cy.visit(baseURL + "products");
-//     cy.wait(2000);
+    cy.visit(baseURL + "products");
+    cy.wait(2000);
 
-//     cy.get(".list-products")
-//       .find("article.mini-product")
-//       .first()
-//       .find('[data-cy="product-link"]')
-//       .click();
+    cy.get(".list-products")
+      .find("article.mini-product")
+      .first()
+      .find('[data-cy="product-link"]')
+      .click();
 
-//     cy.wait(800);
+    cy.wait(800);
 
-//     // Récupérer le nom du produit
-//     cy.get('[data-cy="detail-product-name"]')
-//       .invoke("text")
-//       .then((productName) => {
-//         // Ajouter une quantité négative
-//         cy.get('[data-cy="detail-product-quantity"]').clear().type("-5");
-//         cy.get('[data-cy="detail-product-add"]').click();
-//         cy.wait(800);
+    // Récupérer le nom du produit
+    cy.get('[data-cy="detail-product-name"]')
+      .invoke("text")
+      .then((productName) => {
+        // Ajouter une quantité négative
+        cy.get('[data-cy="detail-product-quantity"]').clear().type("-5");
+        cy.get('[data-cy="detail-product-add"]').click();
+        cy.wait(800);
 
-//         // Vérifier que le produit n'est pas ajouté au panier
-//         cy.visit(baseURL + "cart");
-//         cy.wait(800);
+        // Vérifier que le produit n'est pas ajouté au panier
+        cy.visit(baseURL + "cart");
+        cy.wait(800);
 
-//         cy.get("#cart-content")
-//           .find(".product")
-//           .contains(productName)
-//           .should("not.exist");
-//       });
-//   });
+        cy.get("#cart-content")
+          .find(".product")
+          .contains(productName)
+          .should("not.exist");
+      });
+  });
 
-//   it("Shouldn't add a quantity of 21 to the cart", () => {
-//     cy.visit(baseURL + "login");
-//     cy.getBySel("login-input-username").type(username);
-//     cy.getBySel("login-input-password").type(password);
-//     cy.getBySel("login-submit").click();
-//     cy.wait(2000);
+  it("Shouldn't add a quantity of 21 to the cart", () => {
+    cy.visit(baseURL + "login");
+    cy.getBySel("login-input-username").type(username);
+    cy.getBySel("login-input-password").type(password);
+    cy.getBySel("login-submit").click();
+    cy.wait(2000);
 
-//     cy.visit(baseURL + "products");
-//     cy.wait(2000);
+    cy.visit(baseURL + "products");
+    cy.wait(2000);
 
-//     cy.get(".list-products")
-//       .find("article.mini-product")
-//       .first()
-//       .find('[data-cy="product-link"]')
-//       .click();
+    cy.get(".list-products")
+      .find("article.mini-product")
+      .first()
+      .find('[data-cy="product-link"]')
+      .click();
 
-//     cy.wait(800);
+    cy.wait(800);
 
-//     cy.get('[data-cy="detail-product-name"]')
-//       .invoke("text")
-//       .then((productName) => {
-//         cy.get('[data-cy="detail-product-quantity"]').clear().type("21");
-//         cy.get('[data-cy="detail-product-add"]').click();
-//         cy.wait(800);
+    cy.get('[data-cy="detail-product-name"]')
+      .invoke("text")
+      .then((productName) => {
+        cy.get('[data-cy="detail-product-quantity"]').clear().type("21");
+        cy.get('[data-cy="detail-product-add"]').click();
+        cy.wait(800);
 
-//         cy.visit(baseURL + "cart");
-//         cy.wait(800);
+        cy.visit(baseURL + "cart");
+        cy.wait(800);
 
-//         cy.get("#cart-content")
-//           .find(".product")
-//           .contains(productName)
-//           .then(($item) => {
-//             if ($item.length) {
-//               cy.wait(2000);
-//               cy.get(".product-quantity")
-//                 .wait(1000)
-//                 .find('[data-cy="cart-line-quantity"]')
+        cy.get("#cart-content")
+          .find(".product")
+          .contains(productName)
+          .then(($item) => {
+            if ($item.length) {
+              cy.wait(2000);
+              cy.get(".product-quantity")
+                .wait(1000)
+                .find('[data-cy="cart-line-quantity"]')
 
-//                 .invoke("val")
-//                 .then((cartQuantity) => {
-//                   const quantity = Number(cartQuantity);
-//                   expect(quantity).to.be.lte(20);
-//                 });
-//             } else {
-//               cy.log(
-//                 "Product not added to the cart as the quantity exceeds the allowed limit."
-//               );
-//             }
-//           });
-//       });
-//   });
-// });
+                .invoke("val")
+                .then((cartQuantity) => {
+                  const quantity = Number(cartQuantity);
+                  expect(quantity).to.be.lte(20);
+                });
+            } else {
+              cy.log(
+                "Product not added to the cart as the quantity exceeds the allowed limit."
+              );
+            }
+          });
+      });
+  });
+});
